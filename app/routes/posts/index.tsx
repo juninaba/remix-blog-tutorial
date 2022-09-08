2f969d2d2bab73750b48958cc545d992ec21d547
juninaba <43479094+juninaba@users.noreply.github.com>
2022-09-08 21:03:17 +0900

    Loading Data

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,47 @@
+import {Link, useLoaderData} from "@remix-run/react";
+import {json} from "@remix-run/node";
+
+type Post = {
+    slug: string;
+    title: string;
+}
+
+type LoaderData = {
+    posts: Array<Post>;
+}
+
+export const loader = async () => {
+    return json<LoaderData>({
+        posts: [
+            {
+                slug: "my-first-post",
+                title: "My First Post",
+            },
+            {
+                slug: "90s-mixtape",
+                title: "A Mixtape I Made Just For You",
+            },
+        ],
+    });
+};
+
 export default function Posts() {
-  return (
-    <main>
-      <h1>Posts</h1>
-    </main>
-  );
+    const {posts} = useLoaderData() as LoaderData;
+    return (
+        <main>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((post) =>(
+                    <li key={post.slug}>
+                        <Link
+                            to={post.slug}
+                            className="text-blue-600 underline"
+                        >
+                            {post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
